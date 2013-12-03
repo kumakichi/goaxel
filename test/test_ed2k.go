fa29bb59a3df762fcaf7799c3bba6402f772a424
xiangzhai <xiangzhai83@gmail.com>
2013-12-03 13:51:40 +0800

    use ydonkey emule protocol to test

--- test/test_ed2k.go
+++ test/test_ed2k.go
@@ -23,6 +23,7 @@ import (
     "net"
     "io"
     "os"
+    "time"
     "encoding/binary"
 )
 
@@ -31,41 +32,48 @@ func uuid() []byte {
     b := make([]byte, 16)
     f.Read(b)
     defer f.Close()
-    uuid := fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+    uuid := fmt.Sprintf("%x-%x-%x-%x-%x-%x-%x-%x",
+        b[0:2], b[2:4], b[4:6], b[6:8], b[8:10], b[10:12], b[12:14], b[14:16])
     fmt.Println("DEBUG:", uuid)
     return b
 }
 
+func be32(num int32) {
+    b := make([]byte, 4)
+    binary.BigEndian.PutUint32(b, uint32(num))
+    fmt.Println("DEBUG:", b)
+}
+
 func main() {
+    be32(5)
+
     /* socket connect */
     conn, err := net.Dial("tcp", "88.191.228.66:7111")
-    defer conn.Close()
     if err != nil {
         fmt.Println("ERROR: ", err.Error())
         return;
+    } else {
+        fmt.Println("Connected")
     }
-
-    var i int32 = 64
-    b := make([]byte, 4)
-    binary.LittleEndian.PutUint32(b, uint32(i))
-    fmt.Println("DEBUG:", b)
+    defer conn.Close()
 
     /* socket write */
-    data := []byte{0xe3,
-                   64, 0, 0, 0,
+    data := []byte{0xE3,
+                   0, 0, 0, 43,
                    0x01,
                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                   0, 0, 0, 0,
-                   54, 18,
-                   4, 0, 0, 0,
-                   2, 0x1, 'l', 'e', 's', 'l', 'i', 'e',
-                   3, 0x11, 60, 0, 0, 0, 0, 0, 0, 0,
-                   3, 0x0f, 54, 18, 0, 0, 0, 0, 0, 0,
-                   3, 0x20, 1, 0, 0, 0, 0, 0, 0, 0}
+                   0x7F, 0x00, 0x00, 0x01,
+                   18, 54,
+                   0, 0, 0, 4,
+                   1, 0, 6, 'l', 'e', 's', 'l', 'i', 'e',
+                   0x11, 60,
+                   0x0f, 18, 54,
+                   0x20, 0}
     tmp := uuid()
     for i := 6; i < 22; i++ { data[i] = tmp[i - 6] }
+    fmt.Println("DEBUG:", string(data))
     fmt.Println("DEBUG:", data)
-    fmt.Println("DEBUG:", len(data) - 6)
+    fmt.Println("DEBUG:", len(data) - 5)
     _, err = conn.Write(data)
     if err != nil {
         fmt.Println("ERROR: ", err.Error())
@@ -73,15 +81,17 @@ func main() {
     }
 
     /* socket read */
-    data = make([]byte, 1)
-    resp := ""
+    data = make([]byte, 1024)
     for {
         n, err := conn.Read(data)
         if err != nil {
             if err != io.EOF { panic(err) }
         }
-        resp += string(data[:n])
-        if err == io.EOF { return }
+        if n == 0 {
+            fmt.Println("No data coming")
+        } else {
+            fmt.Println(string(data[:n]))
+        }
+        time.Sleep(1000 * time.Millisecond)
     }
-    fmt.Println("DEBUG:", resp)
 }
